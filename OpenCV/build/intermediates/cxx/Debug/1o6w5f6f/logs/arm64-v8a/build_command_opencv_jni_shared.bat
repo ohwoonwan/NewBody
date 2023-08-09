21e973587b8a4436352d0b51c4f7ee8b3ee89b05
gwanghuiLee <lgh970409@gmail.com>
2023-08-09 13:22:54 +0900

    feat)opencv remove

--- OpenCV/build/intermediates/cxx/Debug/1o6w5f6f/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\jwoo3\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\jwoo3\\AndroidStudioProjects\\NewBody\\OpenCV\\.cxx\\Debug\\1o6w5f6f\\arm64-v8a" ^
-  opencv_jni_shared
--- OpenCV/build/intermediates/cxx/Debug/1o6w5f6f/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\show_\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\workspace\\NewBody\\OpenCV\\.cxx\\Debug\\1o6w5f6f\\x86_64" ^
-  opencv_jni_shared
