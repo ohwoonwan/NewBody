c3819ddeef173f5227880e9347fc0c4059a2f7d5
Jiwoo Yang <111224337+YangjiwooGN@users.noreply.github.com>
2023-08-09 18:56:43 +0900

    feat)posture_ui

---
+++ app/src/main/java/com/example/newbody/posture/PostureInfo.java
@@ -0,0 +1,16 @@
+package com.example.newbody.posture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newbody.R;
+
+public class PostureInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posture_info);
+    }
+}
