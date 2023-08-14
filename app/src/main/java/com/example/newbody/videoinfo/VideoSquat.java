0cbc9b7e4f47273f8c6e119735dfaa8050da74c2
Jiwoo Yang <111224337+YangjiwooGN@users.noreply.github.com>
2023-08-14 22:24:52 +0900

    feat)stt_update

--- app/src/main/java/com/example/newbody/videoinfo/VideoSquat.java
+++ app/src/main/java/com/example/newbody/videoinfo/VideoSquat.java
@@ -1,13 +1,25 @@
 package com.example.newbody.videoinfo;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.widget.VideoView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.newbody.R;
+import com.example.newbody.Video;
+import com.example.newbody.VoiceRecognitionService;
+
+import java.util.ArrayList;
 
 public class VideoSquat extends AppCompatActivity {
 
@@ -16,6 +28,9 @@ public class VideoSquat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_squat);
+
+        Intent intent = new Intent(this, VoiceRecognitionService.class);
+        startService(intent);
 
         mVideoView = findViewById(R.id.squat);
         Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/squat");
@@ -36,6 +51,73 @@ public class VideoSquat extends AppCompatActivity {
                 mVideoView.start();
             }
         });
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int resultCode = intent.getIntExtra("resultCode", Activity.RESULT_CANCELED);
+            if (resultCode == 1) {
+                VoiceTask voiceTask = new VoiceTask();
+                voiceTask.execute();
+            }
+        }
+    };
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK) {
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String str = results.get(0);
+            if(str.equals("이전") || str.equals("나가기")){
+                Intent intent = new Intent(getApplicationContext(), Video.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void restartVoiceRecognitionService() {
+        Intent intent = new Intent(this, VoiceRecognitionService.class);
+        startService(intent);
+    }
+
+    public class VoiceTask extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            getVoice();
+        }
+    }
+
+    private void getVoice() {
+        Intent intent = new Intent();
+        intent.setAction(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        String language = "ko-KR";
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 5000);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 2000);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 브로드캐스트 리시버 등록
+        registerReceiver(receiver, new IntentFilter("com.example.newbody.RESULT_ACTION"));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 브로드캐스트 리시버 등록 해제
+        unregisterReceiver(receiver);
     }
 
 }
