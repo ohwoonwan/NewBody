1c2b878b025289c0df881d5bd35d642bcc635179
ddoonkorea <118863151+ddoonkorea@users.noreply.github.com>
2023-09-02 01:07:59 +0900

    Fail111

--- app/src/main/java/com/example/newbody/RecyclerViewAdapterInvite.java
+++ app/src/main/java/com/example/newbody/RecyclerViewAdapterInvite.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +19,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
 import com.kakao.sdk.common.util.Utility;
 
 import java.security.MessageDigest;
@@ -42,44 +45,56 @@ public class RecyclerViewAdapterInvite extends RecyclerView.Adapter<RecyclerView
         return new ViewHolder(view);
     }
 
-    //친구초대 창에 나오는 정보
+    // 친구초대 창에 나오는 정보
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         FriendInviteContact contact = contactList.get(position);
         holder.nameTextView.setText(contact.getName());
         holder.phoneNumberTextView.setText(contact.getPhoneNumber());
-        String nativeAppKey = context.getString(R.string.kakao_share_app_key);
+
         // 초대 버튼 클릭 리스너 설정
         holder.ivtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("RecyclerViewAdapter", "친구 초대 버튼 클릭");
+                // 친구 초대 창으로 이동하는 Intent 생성
+                Intent inviteIntent = new Intent(context, FriendInvite.class);
+                context.startActivity(inviteIntent);
                 // 카카오톡 앱 링크 생성
                 String packageName = "com.kakao.talk";
-                String dynamicLinkUrl = "https://newbody.page.link/XktS";
+                String dynamicLinkUrl = "https://newbody.page.link/mKfG";
                 String appName = "NewBody"; // 앱 이름
-                String kakaoLink = "kakaotalk://send?text=안녕하세요! 친구 초대 메시지입니다.&url=" + dynamicLinkUrl;
-                Log.d("RecyclerViewAdapter", "카카오톡 링크 URI: " + kakaoLink); // 추가한 라인
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(kakaoLink));
-                intent.setPackage(packageName);
 
-                // 카카오톡 앱이 설치되어 있는지 확인
-                PackageManager packageManager = context.getPackageManager();
-                List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-                boolean isKakaoInstalled = activities.size() > 0;
+                // Firebase Dynamic Links를 통해 동적 링크 생성하기 위한 파라미터 설정
+                DynamicLink.Builder dynamicLinkBuilder = FirebaseDynamicLinks.getInstance()
+                        .createDynamicLink()
+                        .setDomainUriPrefix("https://newbody.page.link") // 파이어베이스에서 설정한 도메인 URL
+                        .setLink(Uri.parse("https://newbody.page.link/mKfG")) // 딥 링크 URL
+                        .setAndroidParameters(new DynamicLink.AndroidParameters.Builder("com.example.newbody") // Android 앱의 패키지명
+                                .setMinimumVersion(1) // 필요에 따라 버전 설정
+                                .build())
+                        .setSocialMetaTagParameters(
+                                new DynamicLink.SocialMetaTagParameters.Builder()
+                                        .setTitle("NewBody")
+                                        .setDescription("친구 초대 메시지입니다.")
+                                        .build());
 
-                if (isKakaoInstalled) {
-                    // 카카오톡 앱이 설치되어 있을 때 카카오톡 앱 실행
+                Uri dynamicLinkUri = dynamicLinkBuilder.buildDynamicLink().getUri(); // 동적 링크 URI 가져오기
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, dynamicLinkUri);
+
+                if (intent.resolveActivity(context.getPackageManager()) != null) {
                     context.startActivity(intent);
-                    Toast.makeText(context, "카카오톡으로 정상적으로 초대 링크를 보냈습니다!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "정상적으로 초대 링크를 보냈습니다!", Toast.LENGTH_SHORT).show();
+
                 } else {
-                    // 카카오톡 앱이 설치되어 있지 않을 때 다른 동작 수행 또는 알림 표시
-                    // 예: "카카오톡 앱을 설치하세요." 메시지 표시
+                    Toast.makeText(context, "초대할 수 있는 앱이 설치되어 있지 않습니다.", Toast.LENGTH_SHORT).show();
                 }
+
             }
+
         });
     }
-
 
     @Override
     public int getItemCount() {
